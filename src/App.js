0b2999f4859981f6854e614180222b92b178173b
Twinkeeyj <68866868+Twinkeeyj@users.noreply.github.com>
2020-12-02 14:21:17 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+
+export default class App extends Component {
+  state = {
+
+  };
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+
+  render() {
+
+    return <p>Юху</p>
+  }
+}
